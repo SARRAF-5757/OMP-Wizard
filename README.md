b033b5c200f1c5efa0af830215481a5ff4e62b4b
SARRAF-5757 <61262059+SARRAF-5757@users.noreply.github.com>
2025-11-09 14:14:53 -0500

    Working checkbox system

--- README.md
+++ README.md
@@ -10,3 +10,5 @@
 - TUI library used - [FTXUI](https://github.com/ArthurSonzogni/FTXUI)
     - [Docs](https://arthursonzogni.github.io/FTXUI/index.html)
 - [Library used](https://github.com/nlohmann/json) to generate JSON
+- Find [Diamonds](https://www.nerdfonts.com/cheat-sheet)
+    - to check what diamonds look like, run ```print '\uef4d'``` for example in the terminal (works with zsh, not sure about other shells)
--- main.cpp
+++ main.cpp
@@ -1,47 +1,53 @@
+#include <iostream>
 #include <ftxui/component/component_base.hpp>
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/screen_interactive.hpp"
 #include "ftxui/dom/elements.hpp"
+#include "nlohmann/json.hpp"
+
+using namespace std;
+using namespace ftxui;
+using json = nlohmann::json;
 
 //* Struct to hold different user choices
 struct ConfigState {
 	//blocks
 	bool show_dir = false;
 	bool show_git = false;
-	//diamonds
-	std::string dmnd_dir_leading = " ";
-	std::string dmnd_git_leading = " ";
-	std::string dmnd_git_trailing = " ";
+    // diamonds (default is )
+    string dmnd_dir_leading = "\uef4d";
+    string dmnd_git_leading = "\uef4d";
+    string dmnd_git_trailing = "\uef4d";
 };
+
+//TODO: Construct a dictionary of diamonds for easier use in the future
 
 int main() {
 	//Initialize Stuff
-	auto screen = ftxui::ScreenInteractive::TerminalOutput();
+	auto screen = ScreenInteractive::TerminalOutput();
 	ConfigState config;
 
-	//Component to render
-    auto toRender = ftxui::Renderer([&] {
-		//
-        ftxui::CheckboxOption opt_path;
-        opt_path.label = "directory path";
-        opt_path.checked = &config.show_dir;
-        ftxui::Component chb_path = ftxui::Checkbox(opt_path);
-
-        ftxui::CheckboxOption opt_git;
-        opt_git.label = "git status";
-        opt_git.checked = &config.show_git;
-        ftxui::Component chb_git = ftxui::Checkbox(opt_git);
+	//Checkboxes to choose which blocks to pick in the prompt
+    auto blocks = Container::Vertical({
+		Checkbox("Directory Path", &config.show_dir),
+		Checkbox("Git Status", &config.show_git),
+		//TODO: add more options
+    });
 
 
+    //Component to render
+    auto toRender = Renderer(blocks, [&] {
         //Display components vertically
-		return ftxui::vbox({
-			ftxui::text("Choose Components to Show in your Prompt") | ftxui::hcenter,
-			chb_path->Render(),
-			chb_git->Render()
+		return vbox({
+			text("Choose Components to Show in your Prompt") | hcenter,
+			blocks->Render() | hcenter
 		});
     });
 
+
     //Loop and render component on the screen
 	screen.Loop(toRender);
+	cout << "Show Directory Path: " << (config.show_dir ? "Yes" : "No") << endl;
+	cout << "Show Git Status: " << (config.show_git ? "Yes" : "No") << endl;
 	return 0;
 }
