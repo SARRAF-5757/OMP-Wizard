5c2a424bd1629db28ad3edebc85869d9f53514e8
chfloyd <christianfloyd313@gmail.com>
2025-11-13 10:02:52 -0500

    Added multiple working tabs

--- main.cpp
+++ main.cpp
@@ -14,8 +14,12 @@ struct ConfigState {
 	//blocks
 	bool show_dir = false;
 	bool show_git = false;
-	bool show_pl1 = false; //TODO: Change
-	bool show_pl2 = false; //TODO: Change
+	bool dmnd_halfC_leading = false;
+	bool dmnd_triangle_leading = false;
+	bool dmnd_halfC_connect = false;
+	bool dmnd_triangle_connect = false;
+	bool dmnd_halfC_trail = false;
+	bool dmnd_triangle_trail = false;
     // diamonds (default is )
     string dmnd_dir_leading = "\uef4d";
     string dmnd_git_leading = "\uef4d";
@@ -32,24 +36,41 @@ int main() {
 	
 	std::vector<std::string> tabMessage = {
 		"Choose Components to Show in your Prompt",
-		"placeholder",
+		"Choose Leading Diamond to Show in your Prompt",
 	};
 	//Blocks representing data within tabs
-    auto tab1Block = Container::Vertical({
+    auto tabShowStatus = Container::Vertical({
 		Checkbox("Directory Path", &config.show_dir),
 		Checkbox("Git Status", &config.show_git),
-		//TODO: add more options
+		//TODO: add more options for TAB1
     });
-	auto tab2Block = Container::Vertical({
-		Checkbox("placeholder1", &config.show_pl1),
-		Checkbox("placeholder2", &config.show_pl2),
-		//TODO: add more options
+	// TODO: Change to radiobox
+	auto tabDmndLead = Container::Vertical({
+		Checkbox("\uf139", &config.dmnd_halfC_leading),
+		Checkbox("\ueb6f", &config.dmnd_triangle_leading),
     });
+	// TODO: Change to radiobox
+	std::string dmndConnect = "Choose Connecting Diamond to Show in your Prompt";
+	auto tabDmndConnect = Container::Vertical({
+		Checkbox("\ue0b4", &config.dmnd_halfC_connect),
+		Checkbox("\ueb70", &config.dmnd_triangle_connect),
+    });
+	// TODO: Change to radiobox
+	std::string dmndTrail = "Choose Trailing Diamond to Show in your Prompt";
+	auto tabDmndTrail = Container::Vertical({
+		Checkbox("\ue0b4", &config.dmnd_halfC_trail),
+		Checkbox("\ueb70", &config.dmnd_triangle_trail),
+    });
+	
+	std::vector<Component> showTabs = {
+		tabShowStatus,
+		tabDmndLead,
+	};	
+
 	
 	//Container of the differing tab blocks
 	auto tabContainer = Container::Tab({
-		tab1Block,
-		tab2Block,
+		showTabs,
 	}, &tabSelected);
 
 	//Container representing the viewed tab
@@ -76,8 +97,22 @@ int main() {
             return true;
         }
 		if (event == Event::Character('n')) {
-			if(tabSelected < 1){
+			if(tabSelected < showTabs.size()-1){
+				if(tabSelected == 0 && config.show_dir && config.show_git){
+					showTabs.push_back(tabDmndConnect);
+					tabMessage.push_back(dmndConnect);
+					showTabs.push_back(tabDmndTrail);
+					tabMessage.push_back(dmndTrail);
+				} else if (tabSelected == 0 && !(config.show_dir && config.show_git)){
+					showTabs.push_back(tabDmndTrail);
+					tabMessage.push_back(dmndTrail);
+				}
 				tabSelected = tabSelected + 1;
+				tabContainer = Container::Tab({
+					showTabs,
+				}, &tabSelected);
+				container->DetachAllChildren();
+				container->Add(tabContainer);
 			}
 		}
         return false;
