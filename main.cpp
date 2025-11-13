d63af6d3a9969debbcf4c4486a36e40f23c1c2aa
SARRAF-5757 <61262059+SARRAF-5757@users.noreply.github.com>
2025-11-13 16:36:59 -0500

    Added radioboxes for diamond choices, implemented block selection check

--- main.cpp
+++ main.cpp
@@ -9,66 +9,79 @@ using namespace std;
 using namespace ftxui;
 using json = nlohmann::json;
 
-//* Struct to hold different user choices
+//^ Struct to hold different user choices
 struct ConfigState {
-	//blocks
+	//block choices
 	bool show_dir = false;
 	bool show_git = false;
-	bool dmnd_halfC_leading = false;
-	bool dmnd_triangle_leading = false;
-	bool dmnd_halfC_connect = false;
-	bool dmnd_triangle_connect = false;
-	bool dmnd_halfC_trail = false;
-	bool dmnd_triangle_trail = false;
-    // diamonds (default is )
-    string dmnd_dir_leading = "\uef4d";
-    string dmnd_git_leading = "\uef4d";
-    string dmnd_git_trailing = "\uef4d";
+    // diamond choices
+    int dmnd_leading = 0;
+    int dmnd_connecting = 0;
+    int dmnd_trailing = 0;
 };
 
-//TODO: Construct a dictionary of diamonds for easier use in the future
+// TODO: Construct a dictionary of diamonds for human-readable usage in the future
+
+//^ List of leading diamond choices (radiobox entries)
+std::vector<std::string> leading_diamonds = {
+    "\ue0b6",	//left half-circle
+    "\ueb6f",	//left triangle
+    "\uf0c8",	//square
+};
+
+//^ List of trailing diamond choices (radiobox entries)
+std::vector<std::string> trailing_diamonds = {
+    "\ue0b4",	//right half-circle
+    "\ueb70",	//right triangle
+    "\uf0c8",	//square
+};
 
 int main() {
 	//Initialize Stuff
 	auto screen = ScreenInteractive::Fullscreen();
 	int tabSelected = 0;
 	ConfigState config;
-	
-	std::vector<std::string> tabMessage = {
-		"Choose Components to Show in your Prompt",
-		"Choose Leading Diamond to Show in your Prompt",
-	};
-	//Blocks representing data within tabs
-    auto tabShowStatus = Container::Vertical({
+
+
+	//* Content inside each tab
+    auto tabChooseBlocks = Container::Vertical({
 		Checkbox("Directory Path", &config.show_dir),
 		Checkbox("Git Status", &config.show_git),
-		//TODO: add more options for TAB1
+		//TODO: add more block choices
     });
-	// TODO: Change to radiobox
-	auto tabDmndLead = Container::Vertical({
-		Checkbox("\uf139", &config.dmnd_halfC_leading),
-		Checkbox("\ueb6f", &config.dmnd_triangle_leading),
-    });
-	// TODO: Change to radiobox
-	std::string dmndConnect = "Choose Connecting Diamond to Show in your Prompt";
-	auto tabDmndConnect = Container::Vertical({
-		Checkbox("\ue0b4", &config.dmnd_halfC_connect),
-		Checkbox("\ueb70", &config.dmnd_triangle_connect),
-    });
-	// TODO: Change to radiobox
-	std::string dmndTrail = "Choose Trailing Diamond to Show in your Prompt";
-	auto tabDmndTrail = Container::Vertical({
-		Checkbox("\ue0b4", &config.dmnd_halfC_trail),
-		Checkbox("\ueb70", &config.dmnd_triangle_trail),
-    });
-	
-	std::vector<Component> showTabs = {
-		tabShowStatus,
-		tabDmndLead,
-	};	
 
+	auto tabDmndLeading = Radiobox({
+		.entries = leading_diamonds,
+		.selected = &config.dmnd_leading,
+    });
 	
-	//Container of the differing tab blocks
+	std::string dmndConnectTitle = "Choose Connecting Diamond to Show in your Prompt";
+    auto tabDmndConnecting = Radiobox({
+		.entries = trailing_diamonds,
+		.selected = &config.dmnd_connecting,
+    });
+
+	std::string dmndTrailTitle = "Choose Trailing Diamond to Show in your Prompt";
+    auto tabDmndTrailing = Radiobox({
+		.entries = trailing_diamonds,
+		.selected = &config.dmnd_trailing,
+    });
+
+
+	//^ Vectors to switch contents among tabs (preloaded with first two screens)
+	//Vector of components representing each tab
+    std::vector<Component> showTabs = {
+		tabChooseBlocks,
+		tabDmndLeading
+	};
+	//Titles shown on each screens
+	std::vector<std::string> tabMessage = {
+		"Choose Components to Show in your Prompt",
+		"Choose Leading Diamond to Show in your Prompt"
+	};
+
+
+    //Container of the differing tab blocks
 	auto tabContainer = Container::Tab({
 		showTabs,
 	}, &tabSelected);
@@ -83,11 +96,17 @@ int main() {
         //Display components vertically
 		return vbox({
 			text(tabMessage[tabSelected]) | hcenter,
+			text(" "),
 			tabContainer->Render() | hcenter,
 			text(" "),
 			text(" "),
-			hbox(text("Quit [q]"), text("          "), text("Next [n]")) | hcenter,
-		});
+			vbox({
+				text("[q] = Quit the wizard without generating config file"),
+				text("[n] = Confirm selections & go to the next screen"),
+				text("[f] = End the wizard and generate config file"),
+				text("      (will use defaults if all options are not specified)")
+			}) | hcenter
+		}) | vcenter;
     });
 
 	//To catch 'q' keypress to exit wizard
@@ -98,23 +117,35 @@ int main() {
         }
 		if (event == Event::Character('n')) {
 			if(tabSelected < showTabs.size()-1){
-				if(tabSelected == 0 && config.show_dir && config.show_git){
-					showTabs.push_back(tabDmndConnect);
-					tabMessage.push_back(dmndConnect);
-					showTabs.push_back(tabDmndTrail);
-					tabMessage.push_back(dmndTrail);
-				} else if (tabSelected == 0 && !(config.show_dir && config.show_git)){
-					showTabs.push_back(tabDmndTrail);
-					tabMessage.push_back(dmndTrail);
-				}
-				tabSelected = tabSelected + 1;
-				tabContainer = Container::Tab({
-					showTabs,
-				}, &tabSelected);
-				container->DetachAllChildren();
-				container->Add(tabContainer);
+				//if only one block is selected, only offer leading and trailing diamond
+				if(tabSelected == 0 && (config.show_dir ^ config.show_git)){
+                    showTabs.push_back(tabDmndTrailing);
+                    tabMessage.push_back(dmndTrailTitle);
+                } else if (tabSelected == 0) {
+				//if multiple blocks are choosen, show all diamond choices
+					showTabs.push_back(tabDmndConnecting);
+                    tabMessage.push_back(dmndConnectTitle);
+					
+                    showTabs.push_back(tabDmndTrailing);
+                    tabMessage.push_back(dmndTrailTitle);
+                }
+				
+				//stop advancing if no blocks are chosen or if at the last page
+				if ((config.show_dir || config.show_git) && (tabSelected < showTabs.size()-1)) {
+                    tabSelected = tabSelected + 1;
+					tabContainer = Container::Tab({
+						showTabs,
+					}, &tabSelected);
+					container->DetachAllChildren();
+					container->Add(tabContainer);
+                }
 			}
 		}
+		if (event == Event::Character('e')) {
+			//TODO: End wizard and generate Json
+            screen.ExitLoopClosure()();	//temp, does the same as quitting
+            return true;
+        }
         return false;
     });
 
